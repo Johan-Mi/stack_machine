dff0ad5438f03bb698f5cf5a1bf4847a20019548
Johan-Mi <johanmi@protonmail.com>
2021-01-17 22:27:52 +0100

    More list stuff

--- src/list.rs
+++ src/list.rs
@@ -4,8 +4,14 @@ use std::rc::Rc;
 
 #[derive(Clone)]
 pub enum List {
-    Cons { value: Rc<Value>, next: Rc<List> },
+    Cons { value: Value, next: Rc<List> },
     None,
+}
+
+impl List {
+    pub fn iter(&self) -> ListIter {
+        ListIter::new(self)
+    }
 }
 
 impl Display for List {
@@ -25,5 +31,43 @@ fn list_to_string(this: &List) -> String {
             format!(" {}{}", value, list_to_string(next))
         }
         List::None => String::from(""),
+    }
+}
+
+#[macro_export]
+macro_rules! make_list {
+    () => {
+        $crate::list::List::None
+    };
+
+    ($first:expr $(, $rest:expr)*) => {
+        $crate::list::List::Cons {
+            value: $first,
+            next: ::std::rc::Rc::new(make_list!($($rest),*)),
+        }
+    };
+}
+
+pub struct ListIter<'a> {
+    node: &'a List,
+}
+
+impl<'a> ListIter<'a> {
+    pub fn new(list: &'a List) -> Self {
+        Self { node: list }
+    }
+}
+
+impl<'a> Iterator for ListIter<'a> {
+    type Item = &'a Value;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.node {
+            List::Cons { value, next } => {
+                self.node = next;
+                Some(value)
+            }
+            List::None => None,
+        }
     }
 }
--- src/value.rs
+++ src/value.rs
@@ -1,11 +1,10 @@
 use super::instruction::Instruction;
 use super::list::List;
 use derive_more::Display;
-use std::rc::Rc;
 
 #[derive(Clone, Display)]
 pub enum Value {
     Int(i32),
-    List(List),
+    List(Box<List>),
     Instruction(Instruction),
 }
