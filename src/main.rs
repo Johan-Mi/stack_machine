21e228a8145c8b43cb96e44caab11d48f17dc554
Johan-Mi <johanmi@protonmail.com>
2023-05-01 14:19:52 +0200

    Enable more lints

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,22 @@
 #![forbid(unsafe_code)]
+#![warn(clippy::nursery, clippy::pedantic)]
 
 mod instruction;
 mod machine;
 mod value;
-use instruction::Instruction::*;
 use machine::Machine;
-use value::Value::*;
 
 fn main() {
+    use instruction::Instruction as I;
+    use value::Value as V;
     let program = vec![
-        Nop,
-        Push(Int(40)),
-        Push(Int(4)),
-        Push(Int(2)),
-        Push(List(vec![Dup, Pop, Sub, Add])),
-        Exec,
-        Print,
+        I::Nop,
+        I::Push(V::Int(40)),
+        I::Push(V::Int(4)),
+        I::Push(V::Int(2)),
+        I::Push(V::List(vec![I::Dup, I::Pop, I::Sub, I::Add])),
+        I::Exec,
+        I::Print,
     ];
 
     let mut machine = Machine::new(program.into_iter());
