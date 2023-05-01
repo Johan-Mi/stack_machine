1753370eef643541cb81105e6fa48cc9294572ae
Johan-Mi <johanmi@protonmail.com>
2023-05-01 13:39:49 +0200

    Prefer using `Self`

--- src/instruction.rs
+++ src/instruction.rs
@@ -14,13 +14,13 @@ pub enum Instruction {
 impl Display for Instruction {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         f.write_str(match self {
-            Instruction::Nop => "nop",
-            Instruction::Pop => "pop",
-            Instruction::Dup => "dup",
-            Instruction::Print => "print",
-            Instruction::Add => "add",
-            Instruction::Sub => "sub",
-            Instruction::Exec => "exec",
+            Self::Nop => "nop",
+            Self::Pop => "pop",
+            Self::Dup => "dup",
+            Self::Print => "print",
+            Self::Add => "add",
+            Self::Sub => "sub",
+            Self::Exec => "exec",
         })
     }
 }
